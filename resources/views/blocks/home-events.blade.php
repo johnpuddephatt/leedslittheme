4ab5e990f5485ed403a30378984276db78814222
John Puddephatt <john@jdp.org.uk>
2025-03-17 18:08:49 +0000

    bg blue

--- resources/views/blocks/home-events.blade.php
+++ resources/views/blocks/home-events.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
       <h2 class="skip-none type-2xl mt-0 font-serif text-white underline decoration-1">{{ $heading ?: 'Events' }}</h2>
 
-      <div class="my-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <div class="my-12 grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-y-12">
         @foreach ($events as $event)
           <x-event-card bg="bg-white" :variant="$loop->first ? 'large' : 'default'" :event="$event" />
         @endforeach
