a2489e7c9cf51e7614f409eb9800dc8e877ca3b4
John Puddephatt <john@jdp.org.uk>
2025-03-26 23:17:26 +0000

    speed up hero

--- resources/views/blocks/home-hero.blade.php
+++ resources/views/blocks/home-hero.blade.php
@@ -14,7 +14,7 @@
     class="bg-radial pointer-events-none absolute inset-0 z-20 from-transparent from-50% to-white/30 opacity-0 delay-1000 duration-[3000ms]">
   </div>
   <div id="content"
-    class="relative flex flex-col items-center justify-center opacity-0 transition-opacity delay-1000 duration-[6000ms]">
+    class="relative flex flex-col items-center justify-center opacity-0 transition-opacity duration-[4000ms]">
     <x-logo class="mb-12 w-24 md:w-40" />
 
     <h1 class="text-center font-serif text-3xl md:text-4xl lg:text-5xl">
